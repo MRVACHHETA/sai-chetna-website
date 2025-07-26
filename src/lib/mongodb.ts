5f267917c88cdcfe9523698d4dedbffd55d2c538
Harsh Vachheta <133560386+MRVACHHETA@users.noreply.github.com>
2025-07-27 00:41:52 +0530

    Fix ESLint error on mongodb.ts for Vercel

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,17 +4,14 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
-
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-client = new MongoClient(uri!, options);
-clientPromise = client.connect();
+const client = new MongoClient(uri!, options);
+const clientPromise = client.connect();
 
 export async function connectToDatabase() {
   const client = await clientPromise;
-  return client.db("sai-chetna-db"); // 👈 this must match your actual database name
+  return client.db("sai-chetna-db"); // 👈 match your DB name
 }
