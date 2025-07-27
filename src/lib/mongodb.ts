7d7ab4bf4bf5af9be984ffea33160b9f62636522
Harsh Vachheta <133560386+MRVACHHETA@users.noreply.github.com>
2025-07-27 17:53:22 +0530

    fix layout oversizing and network login issue

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 const options = {};
-
+console.log("DEBUG URI:", process.env.MONGODB_URI);
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
